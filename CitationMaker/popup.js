d8602e8574a57e65694fbd09d0709bf7c91a0121
ryanpears <pryan78@yahoo.com>
2020-05-18 14:52:51 -0600

    can display a dumby attribute for and a few normal code changes

--- CitationMaker/popup.js
+++ CitationMaker/popup.js
@@ -1,17 +1,42 @@
 
 document.addEventListener('DOMContentLoaded', function(){//waits for everything to load
     let buttonMla = document.getElementById("MLA");
-   buttonMla.addEventListener('click',onclick,false);//button click listener
+    let buttonManual = document.getElementById("manual");
+    let manualEntry = document.getElementById("manualEntries");//the entry boxes
 
-   onclick = function(){
-       chrome.tabs.query({currentWindow: true, active: true},
-           function(tabs){//callback after getting tab
-           //alert("got the tab");
-               chrome.tabs.sendMessage(tabs[0].id,{type :"MLA"},displayResponse);
-           });
+    manualEntry.style.display = "none";//default to hidden
 
-   }
+    //buttonMla.addEventListener('click',onclick,false);//button click listener
+    //registers the listeners
+    buttonMla.onclick = function(){
+        //alert("this still works");
+        chrome.tabs.query({currentWindow: true, active: true},
+            function(tabs){//callback after getting tab
+                //alert("got the tab");
+                chrome.tabs.sendMessage(tabs[0].id,{type :"MLA"},displayResponse);
+            });
+
+    };
+
+    buttonManual.onclick = function(){
+        //hides the manual entries.
+        if(manualEntry.style.display != "none"){
+            manualEntry.style.display = "none";//this may change not sure the best one yet grid or table could work
+        }else{
+            manualEntry.style.display = "block";//this may change not sure the best one yet grid or table could work
+        }
+        //creates the elements to allow manual entry
+        /*TODO somehow needs to real time update what the citation looks like.
+         * probably open a different connection with background.js so mulitple messages can be sent and
+         * received this will be pretty cool.
+         */
+    };
+
+
+
+
 },false);
+
 
 /**
  * displays the citation
@@ -19,4 +44,4 @@ document.addEventListener('DOMContentLoaded', function(){//waits for everything 
  */
 displayResponse = function(response){
     document.getElementById("citeDisp").innerHTML = response.citation;
-}
+};
